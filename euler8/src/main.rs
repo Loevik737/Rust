e2ea8e7c7b028bc0d186bb7117258919b3118a96
haavaal <haavaal@stud.ntnu.no>
2017-05-27 15:21:25 +0200

    started on euler problem 9

---
+++ euler8/src/main.rs
@@ -0,0 +1,19 @@
+/*
+A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
+
+a2 + b2 = c2
+For example, 32 + 42 = 9 + 16 = 25 = 52.
+
+There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+Find the product abc.
+*/
+
+extern crate time;
+use time::PreciseTime;
+
+fn main() {
+    let start = PreciseTime::now();
+    println!("{:?}", "hei");
+    let end = PreciseTime::now();
+    println!("Used: {} seconds", start.to(end));
+}
