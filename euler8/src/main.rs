a1530831e0ea1c12dceb62fe25d59f07cf365693
Håvard Aspen Løvik <hloevi@live.no>
2017-06-11 14:03:42 +0200

    finished with problem 9

--- euler8/src/main.rs
+++ euler8/src/main.rs
@@ -13,7 +13,21 @@ use time::PreciseTime;
 
 fn main() {
     let start = PreciseTime::now();
-    println!("{:?}", "hei");
+    println!("Triplet and product[a,b,c,prod] where a+b+c = {0}: {1:?}",1000, pythagorean_triplet(1000_f64));
     let end = PreciseTime::now();
     println!("Used: {} seconds", start.to(end));
+}
+
+fn pythagorean_triplet(n:f64) -> Vec<Vec<f64>>{
+    let mut a = 0.0;
+    let mut b = 0.0;
+    let mut result = Vec::new();
+    for i in 2..(((n/2.0).floor()/2.0) as i32){
+        a = n*((n/2.0) - (i as f64))/(n-(i as f64));
+        if a > 0.0 && a - a.floor() == 0.0  {
+            b = i as f64;
+            result.push(vec![a,b,n-a-b,a*b*(n-a-b)]);
+        }
+    }
+    result
 }
