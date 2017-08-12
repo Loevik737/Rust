3e5d1b0dc6a45d2a044bd498a9e835db226a65eb
haavaal <haavaal@stud.ntnu.no>
2017-08-12 14:03:50 +0200

    merging

--- euler8/src/main.rs
+++ euler8/src/main.rs
@@ -1,8 +1,8 @@
 /*
 A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
 
-a2 + b2 = c2
-For example, 32 + 42 = 9 + 16 = 25 = 52.
+a^2 + b^2 = c^2
+For example, 3^2 + 4^2 = 9 + 16 = 25 = 5^2.
 
 There exists exactly one Pythagorean triplet for which a + b + c = 1000.
 Find the product abc.
@@ -13,7 +13,18 @@ use time::PreciseTime;
 
 fn main() {
     let start = PreciseTime::now();
-    println!("{:?}", "hei");
+    println!("{:?}", triplet_product());
     let end = PreciseTime::now();
     println!("Used: {} seconds", start.to(end));
+}
+
+fn triplet_product() -> u32 {
+    let mut product:u32 = 0;
+    let mut a:u32 = 0;
+    let mut b:u32 = 0;
+    let mut c:u32 = 0; 
+    for 500..0{
+        if
+    }
+    product
 }
